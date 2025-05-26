83b3a0e683486da9cc482202f3043ccc105deb0c
hasanciftci26 <hasanciftcii26@gmail.com>
2025-05-26 11:54:22 +0200

    Library consumption configs

--- src/library.ts
+++ src/library.ts
@@ -2,13 +2,12 @@ import Lib from "sap/ui/core/Lib";
 
 const library = Lib.init({
     name: "ui5.antares.pro",
+    apiVersion: 2,
     dependencies: [
         "sap.ui.core",
         "sap.m"
     ],
-    controls: [
-        "ui5.antares.pro.core.v2.Root"
-    ],
+    controls: [],
     noLibraryCSS: true,
     version: "1.0.0"
 });
