636b1647a59347b0560187640c3c135f384bda59
hasanciftci26 <hasanciftcii26@gmail.com>
2025-06-18 16:52:46 +0200

    Custom Controls

--- src/v2/custom/control/ControlGenerator.ts
+++ src/v2/custom/control/ControlGenerator.ts
@@ -1,8 +1,12 @@
 import ManagedObject from "sap/ui/base/ManagedObject";
+import { ClassMetadata } from "ui5/antares/pro/types/Global.types";
 
 /**
  * @namespace ui5.antares.pro.v2.custom.control
  */
 export default class ControlGenerator extends ManagedObject {
-    
+    static metadata: ClassMetadata = {
+        library: "ui5.antares.pro",
+        final: true
+    };
 }
